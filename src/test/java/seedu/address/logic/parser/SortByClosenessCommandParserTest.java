23053f87ca578b1cc7e97bbbe3f114ada1c3d074
Gokul <gokulfan123@gmail.com>
2025-10-23 18:10:03 +0800

    Add tests for SortByCloseness and its Parser

---
+++ src/test/java/seedu/address/logic/parser/SortByClosenessCommandParserTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.SortByClosenessCommand;
+
+public class SortByClosenessCommandParserTest {
+    private SortByClosenessCommandParser parser = new SortByClosenessCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSortByClosenessCommand() {
+        assertParseSuccess(parser, "asc", new SortByClosenessCommand(SortByClosenessCommand.SortOrder.ASCENDING));
+        assertParseSuccess(parser, "desc", new SortByClosenessCommand(SortByClosenessCommand.SortOrder.DESCENDING));
+    }
+
+    @Test
+    public void parse_invalidValue_throwsParseException() {
+        // Test for invalid order
+        String invalidOrder = "invalid";
+        String expectedMessage = SortByClosenessCommand.MESSAGE_INVALID_ORDER;
+        assertParseFailure(parser, invalidOrder, expectedMessage);
+
+        // Test for empty order
+        String emptyInput = "   ";
+        assertParseFailure(parser, emptyInput, expectedMessage);
+    }
+}
